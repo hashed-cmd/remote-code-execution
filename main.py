b60bdfa8d602f0c3ed01e30e34d33a577ef96fdf
d357fc1f66406a0194404f9fdb649709 <artificiallence+hash@proton.me>
2025-06-30 19:46:00 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/getscript")
+def get_script():
+    lua_script = """
+game.StarterGui:SetCore("SendNotification", {
+    Title = "hashed";
+    Text = "Working";
+    Duration = 5;
+})
+"""
+    return lua_script, 200, {"Content-Type": "text/plain"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005)
